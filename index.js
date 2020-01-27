4646fb113adfb74cfb47209c396fafabacdf67a6
Alexandra Pestruyeva <apestruy@gmail.com>
2020-01-27 23:43:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var customerName = "bob"
 
 function upperCaseCustomerName() {
- customerName = customerName.toUpperCase()
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"
 }
